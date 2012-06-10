96f25d26c06c8a6019742e6df4d34f5fa221cba3
Steve Vlaminck <steve@refactr.com>
2012-06-10 15:08:42 -0500

    Added tests

---
+++ test/unit/spectrum/TransactionItemTests.groovy
@@ -0,0 +1,17 @@
+package spectrum
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(TransactionItem)
+class TransactionItemTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
